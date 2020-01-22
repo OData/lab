50a3846c88bdb51aca11e5cf872a137ee4d187be
Clement Habinshuti <clhabins@microsoft.com>
2020-01-22 19:17:13 +0300

    Add tests for ODataConnectedServiceHander

---
+++ ODataConnectedService/src/CodeGeneration/ICodeGenDescriptorFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using EnvDTE;
+using Microsoft.VisualStudio.ConnectedServices;
+
+namespace Microsoft.OData.ConnectedService.CodeGeneration
+{
+    interface ICodeGenDescriptorFactory
+    {
+        BaseCodeGenDescriptor Create(Version edmxVersion, string metadataUri, ConnectedServiceHandlerContext context, Project project);
+    }
+}
