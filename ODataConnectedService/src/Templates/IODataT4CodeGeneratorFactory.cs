45ca8e1521038cfbbcf9cab5d053831bc529e8d8
Clement Habinshuti <clhabins@microsoft.com>
2020-01-22 11:57:21 +0300

    Add tests for V4CodeGenDescriptor

---
+++ ODataConnectedService/src/Templates/IODataT4CodeGeneratorFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.OData.ConnectedService.Templates
+{
+    interface IODataT4CodeGeneratorFactory
+    {
+        ODataT4CodeGenerator Create();
+    }
+}
